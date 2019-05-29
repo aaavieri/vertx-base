e24050915c62120b1773651cc3f3e2d18b3d82c1
jiaolongyin <aaavieri@gmail.com>
2019-05-30 00:06:33 +0800

    add some function

--- vertx-base-web/src/main/java/com/yjl/vertx/base/web/util/ContextUtil.java
+++ vertx-base-web/src/main/java/com/yjl/vertx/base/web/util/ContextUtil.java
@@ -1,5 +1,6 @@
 package com.yjl.vertx.base.web.util;
 
+import com.yjl.vertx.base.com.util.JsonUtil;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import lombok.AccessLevel;
@@ -29,5 +30,31 @@ public class ContextUtil {
     
     public static JsonObject cpContextStrToJson(JsonObject jsonObject, RoutingContext routingContext, String... keys) {
         return ContextUtil.<String>cpContextToJson(jsonObject, routingContext, keys);
+    }
+    
+    public static JsonObject getAllParams(RoutingContext context) {
+        return Stream.of(getParam(context), getFormAttrs(context), getJsonBOdy(context))
+            .reduce((jsonObject1, jsonObject2) -> jsonObject1.mergeIn(jsonObject2, true))
+            .orElse(new JsonObject());
+    }
+    
+    public static JsonObject getParam(RoutingContext context) {
+        JsonObject retData = new JsonObject();
+        context.request().params().forEach(entry -> retData.put(entry.getKey(), entry.getValue()));
+        return retData;
+    }
+    
+    public static JsonObject getFormAttrs(RoutingContext context) {
+        JsonObject retData = new JsonObject();
+        context.request().formAttributes().forEach(entry -> retData.put(entry.getKey(), entry.getValue()));
+        return retData;
+    }
+    
+    public static JsonObject getJsonBOdy(RoutingContext context) {
+        JsonObject retData = new JsonObject();
+        if (JsonUtil.isJson(context.getBodyAsString())) {
+            context.getBodyAsJson().getMap().forEach(retData::put);
+        }
+        return retData;
     }
 }
